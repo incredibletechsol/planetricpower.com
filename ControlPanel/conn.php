ff77e8636a7f0d2e58f725114d592cafc2bbb621
Amit Bhalerao <amitbaramatimca@gmail.com>
2021-08-07 13:58:48 +0530

    Final data

--- ControlPanel/conn.php
+++ ControlPanel/conn.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbName = "infinityam";
+$username = "planetri_planetric";
+$password = "Planetric12#$";
+$dbName = "planetri_planetric";
 
 // Create connection
 $conn =  mysqli_connect($servername, $username, $password, $dbName);
